83b3caee8343420592a6c1b51472d3ca78b716ac
Marcin Popławski <marpop8@gmail.com>
2021-06-23 23:15:36 +0200

    Change ALLOWED_HOSTS

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="A1ngs8r2vrpjzLdz2Ao7AaaeErMB3ikDvAQLRrLYOSHUnBB6zjhrjgocc4g3BU73",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,7 @@ from config.settings.base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["demo-car-app.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 # SECURITY
 # ------------------------------------------------------------------------------
